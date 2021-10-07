9ff365be65d5b2ec08091acba770172f04183789
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-10-07 19:39:43 +0000

    Estructura hecha

--- src/app.js
+++ src/app.js
@@ -7,5 +7,56 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".suit").innerHTML = suitRandom();
+  document.querySelector(".num").innerHTML = numRandom();
+  //document.querySelector(".card").innerHTML = cardGenerate() + suitRandom();
 };
+function cardGenerate() {
+  const suit = ["♦", "♥", "♠", "♣"];
+  const num = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  if (suit[random(suit)] === "♦" || suit[random(suit)] === "♦") {
+    //document.querySelector.suit[random(suit)];
+  }
+  return suit[random(suit)] + num[random(num)];
+  //console.log(suit[random(suit)] + num[random(num)]);
+}
+function suitRandom() {
+  const suit = ["♦", "♥", "♠", "♣"];
+  return suit[random(suit)];
+}
+function numRandom() {
+  const num = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  return num[random(num)];
+}
+
+function random(arr1) {
+  return Math.floor(Math.random() * arr1.length);
+}
