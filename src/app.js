38f885a475ed9237343ea65891512a7a21bcdaaf
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-10-09 10:23:27 +0000

    changes 2

--- src/app.js
+++ src/app.js
@@ -7,37 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  document.querySelector(".suit").innerHTML = suitRandom();
+
+  //document.querySelector(".suit").innerHTML = suitRandom();
   document.querySelector(".num").innerHTML = numRandom();
+  let suitDiv = document.querySelectorAll(".suit");
+  suitDiv[0].innerHTML = suit;
+  suitDiv[1].innerHTML = suit;
   //document.querySelector(".card").innerHTML = cardGenerate() + suitRandom();
 };
-function cardGenerate() {
-  const suit = ["♦", "♥", "♠", "♣"];
-  const num = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  if (suit[random(suit)] === "♦" || suit[random(suit)] === "♦") {
-    //document.querySelector.suit[random(suit)];
-  }
-  return suit[random(suit)] + num[random(num)];
-  //console.log(suit[random(suit)] + num[random(num)]);
+let suits = ["♦", "♥", "♠", "♣"];
+
+if (random(suits) === 1 || random(suits) === 2) {
+  var suit = "<p style ='color:red'>" + suits[random(suits)] + "</p>";
+} else {
+  var suit = "<p style ='color:black'>" + suits[random(suits)] + "</p>";
 }
-function suitRandom() {
-  const suit = ["♦", "♥", "♠", "♣"];
-  return suit[random(suit)];
-}
+
 function numRandom() {
   const num = [
     "A",
